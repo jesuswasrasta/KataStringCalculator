55887966cf1ec5ba1febc3ca6343add9a7994575
Ferdinando Santacroce <jesus_was_rasta@yahoo.it>
2016-05-04 08:59:58 +0200

    Solution stub with first tests

---
+++ StringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using NUnit.Framework;
+
+
+namespace StringCalculator.Tests
+{
+	[TestFixture]
+    public class CalculatorTests
+    {
+		[Test]
+		public void EmptyString_Returns_0()
+		{
+			var calculator = new Calculator();
+			var actualResult = calculator.Add(String.Empty);
+
+			Assert.That(actualResult, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void NonNumericString_Returns_0()
+		{
+			var calculator = new Calculator();
+			var actualResult = calculator.Add("abc");
+
+			Assert.That(actualResult, Is.EqualTo(0));
+		}
+
+		[Test]
+		public void StringWithNumber3_Returns_3()
+		{
+			var calculator = new Calculator();
+			var actualResult = calculator.Add("3");
+
+			Assert.That(actualResult, Is.EqualTo(3));
+		}
+
+		[Test]
+		public void StringWithNumber3comma4_Returns_7()
+		{
+			var calculator = new Calculator();
+			var actualResult = calculator.Add("3,4");
+
+			Assert.That(actualResult, Is.EqualTo(7));
+		}
+	}
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+	    public int Add(string input)
+	    {
+			throw new NotImplementedException();
+	    }
+    }
+}
